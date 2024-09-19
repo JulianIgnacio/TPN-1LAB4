b62ff58800aaa42b7e6a68fbd91ad060dd7d0d7c
JulianIgnacio <montenegrojulianig@gmail.com>
2024-09-19 08:48:07 -0300

    se completo el ejercicio 11

---
+++ Ejercicio11.py
@@ -0,0 +1,10 @@
+alumnos = 50
+soloFresa=10
+soloChocolate=5
+ningunoDeLosDos=20
+ambos=alumnos-soloChocolate-soloChocolate-ningunoDeLosDos
+totalSoloFresa=soloFresa+ambos
+TotalSoloChocolate=soloChocolate+ambos
+print(f"los chicos que le gustan los dos gustos de helado son: {ambos}")
+print(f"El total de chicos que le gustan el helado de fresa es:{totalSoloFresa}")
+print(f"El total de chicos que le gusta el helado de chocolate es: {TotalSoloChocolate}")
